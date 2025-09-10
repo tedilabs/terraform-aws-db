0e6a753dc437cb73dc5b0c67d834ee94a108d4a1
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2025-09-11 00:19:18 +0900

    feat: improve how to use resource_group in modules (#37)

--- modules/elasticache-redis-cluster/outputs.tf
+++ modules/elasticache-redis-cluster/outputs.tf
@@ -181,3 +181,19 @@ output "endpoints" {
     configuration = aws_elasticache_replication_group.this.configuration_endpoint_address
   }
 }
+
+output "resource_group" {
+  description = "The resource group created to manage resources in this module."
+  value = merge(
+    {
+      enabled = var.resource_group.enabled && var.module_tags_enabled
+    },
+    (var.resource_group.enabled && var.module_tags_enabled
+      ? {
+        arn  = module.resource_group[0].arn
+        name = module.resource_group[0].name
+      }
+      : {}
+    )
+  )
+}
--- modules/elasticache-redis-user-group/outputs.tf
+++ modules/elasticache-redis-user-group/outputs.tf
@@ -22,3 +22,19 @@ output "users" {
   description = "The list of user IDs that belong to the user group."
   value       = values(aws_elasticache_user_group_association.this)[*].user_id
 }
+
+output "resource_group" {
+  description = "The resource group created to manage resources in this module."
+  value = merge(
+    {
+      enabled = var.resource_group.enabled && var.module_tags_enabled
+    },
+    (var.resource_group.enabled && var.module_tags_enabled
+      ? {
+        arn  = module.resource_group[0].arn
+        name = module.resource_group[0].name
+      }
+      : {}
+    )
+  )
+}
